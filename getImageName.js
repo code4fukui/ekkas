187262665b3a4d3468b30c452472d8e18ff2995d
Taisuke Fukuno <fukuno@jig.jp>
2025-12-10 18:39:23 +0900

    fix age calc

--- getImageName.js
+++ getImageName.js
@@ -1,12 +1,15 @@
 export const funcfn = (p) => `img/face_${p[0]}${p[1]}_${p[2]}.png`;
 
 const getAge = (age_or_byear, year) => {
+  if (!year) {
+    year = new Date().getFullYear();
+  }
   age_or_byear = parseInt(age_or_byear);
   if (age_or_byear < 1700) {
     return age_or_byear;
   }
   const y = year - age_or_byear;
-  for (let i = 10; i <= 80; i++) {
+  for (let i = 10; i <= 80; i += 10) {
     if (y < i + 10) return i;
   }
   return 80;
--- getImageName.test.js
+++ getImageName.test.js
@@ -1,10 +1,7 @@
-import { CSV } from "https://js.sabae.cc/CSV.js";
+import * as t from "https://deno.land/std/testing/asserts.ts";
 import { getImageName } from "./getImageName.js";
 
-const data = await CSV.fetchJSON("https://code4fukui.github.io/opendata-hokuriku/merged_survey.csv");
-for (const d of data) {
-  const fn = getImageName(d.性別, d.年代, "normal"); // "happy", "sad", "normal"
-  console.log(fn);
-  if (!fn) throw new Error(d.性別 + " " + d.年代);
-}
-
+Deno.test("simple", () => {
+  const fn = getImageName("男", 2001, "normal"); // "happy", "sad", "normal"
+  t.assertEquals(fn, "img/face_m20_normal.png");
+});
